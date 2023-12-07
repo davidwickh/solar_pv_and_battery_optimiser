eb336d3fd39517537df057ef234066fa35b97916
davidwickh <56641696+davidwickh@users.noreply.github.com>
2023-12-07 21:42:04 +0000

    Moving the optimisation variables into their own class

--- model/linear_optimiser/optimiser.py
+++ model/linear_optimiser/optimiser.py
@@ -1,3 +1,5 @@
+import logging
+from pathlib import Path
 from typing import Optional
 
 import matplotlib.pyplot as plt
@@ -6,6 +8,9 @@ import pulp as pl
 from pulp import LpStatus
 
 from model.constants import ENERGY_DEMAND, SOLAR_IRRADIANCE, OptimisationObjectives
+from model.linear_optimiser.variables import OptimiserVariables
+
+logger = logging.getLogger(__name__)
 
 
 class Optimiser:
@@ -14,214 +19,239 @@ class Optimiser:
     """
 
     problem: pl.LpProblem = None
+    variables: OptimiserVariables
+
+    def __init__(
+            self,
+            energy_demand: pd.DataFrame,
+            solar_irradiance: pd.DataFrame,
+            time_slices: range,
+            optimisation_objective: OptimisationObjectives,
+            solar_size: float = None,
+            solar_capex: Optional[float] = None,
+            battery_capex: Optional[float] = None,
+    ):
+        """
+        Initialises the optimiser by creating the optimiser variables.
+        :param solar_irradiance: Solar irradiance profile (W/m2)
+        :param energy_demand: Energy demand profile (kWh)
+        :param time_slices: Time slices to optimise over
+        :param optimisation_objective: Optimisation objective
+        """
+        self.energy_demand = energy_demand
+        self.solar_irradiance = solar_irradiance
+        self.variables = OptimiserVariables.create_variables(
+            time_slices=time_slices,
+            optimisation_objective=optimisation_objective,
+            solar_size=solar_size,
+        )
+        self.optimisation_objective = optimisation_objective
+        self.time_slices = time_slices
+        self.solar_capex = solar_capex
+        self.battery_capex = battery_capex
 
     def create_optimisation_problem(
-        self,
-        energy_demand: pd.DataFrame,
-        solar_irradiance: pd.DataFrame,
-        battery_initial_capacity: float,
-        battery_degradation_rate: float,
-        optimisation_objective: OptimisationObjectives,
-        time_slices: range,
-        solar_capacity: Optional[float] = None,
-        solar_efficiency: Optional[float] = 0.15,
+            self,
+            battery_initial_capacity: float,
+            battery_degradation_rate: float,
+            solar_efficiency: Optional[float] = 0.15,
     ) -> None:
         """
-        Creates the optimisation problem.
+        Main entry point to the optimiser. Calls the following functions:
+            - define the object function
+            - define the constraints
+        :param battery_initial_capacity: Initial battery capacity (kWh)
+        :param battery_degradation_rate: Battery degradation rate, in (kWh/kWh)
+        :param battery_capex: Battery capex (£/kWh)
+        :param solar_capex: Solar capex (£/m2)
+        :param solar_efficiency: Solar efficiency (%). Default is 15%.
         :return: None
         """
-        # Define the problem
-        if optimisation_objective == OptimisationObjectives.MINIMISE_BATTERY_CAP:
-            self.problem = pl.LpProblem("BatterySizeMinimisation", pl.LpMinimize)
-        elif (
-            optimisation_objective
-            == OptimisationObjectives.MINIMISE_BATTERY_AND_SOLAR_COST
-        ):
-            self.problem = pl.LpProblem("BatteryAndPVCostMinimisation", pl.LpMinimize)
-        else:
-            raise ValueError(
-                f"Unknown optimisation objective: {optimisation_objective}"
-            )
-
-        # Define variables
-        # Renewable electricity flow
-        renewable_electricity_to_house = pl.LpVariable.dicts(
-            name="renewable_electricity_to_house",
-            indices=time_slices,
-            lowBound=0,
-            cat="Continuous",
+        self._define_problem()
+        self._define_objective_function(
+            battery_capex=self.battery_capex,
+            solar_capex=self.solar_capex,
         )
-        renewable_electricity_to_battery = pl.LpVariable.dicts(
-            name="renewable_electricity_to_battery",
-            indices=time_slices,
-            lowBound=0,
-            cat="Continuous",
-        )
-        # Battery electricity flow
-        battery_electricity_to_house = pl.LpVariable.dicts(
-            name="battery_electricity_to_house",
-            indices=time_slices,
-            lowBound=0,
-            cat="Continuous",
-        )
-        # Battery state of charge
-        battery_state_of_charge = pl.LpVariable.dicts(
-            name="battery_state_of_charge",
-            indices=time_slices,
-            lowBound=0,
-            cat="Continuous",
-        )
-        # Battery capacity
-        battery_capacity = pl.LpVariable(
-            name="battery_capacity",
-            lowBound=0,
-            cat="Continuous",
-        )
-        if (
-            optimisation_objective
-            == OptimisationObjectives.MINIMISE_BATTERY_AND_SOLAR_COST
-        ):
-            # Solar capacity
-            solar_size = pl.LpVariable(
-                name="solar_capacity",
-                lowBound=0,
-                cat="Continuous",
-            )
-        elif optimisation_objective == OptimisationObjectives.MINIMISE_BATTERY_CAP:
-            solar_size = solar_capacity
-        else:
-            raise ValueError(
-                f"Unknown optimisation objective: {optimisation_objective}"
-            )
-
-        # Objective function
-        if optimisation_objective == OptimisationObjectives.MINIMISE_BATTERY_CAP:
-            self.problem += (
-                pl.lpSum(battery_capacity),
-                "Objective - minimise battery size",
-            )
-        elif (
-            optimisation_objective
-            == OptimisationObjectives.MINIMISE_BATTERY_AND_SOLAR_COST
-        ):
-            raise NotImplementedError
-        else:
-            raise ValueError(
-                f"Unknown optimisation objective: {optimisation_objective}"
-            )
-
-        # Energy flow constraints
-        for _t in time_slices:
+        # Define the constraints
+        for _t in self.time_slices:
             # Solar generation
             solar_generation = (
-                solar_irradiance[SOLAR_IRRADIANCE][_t]
-                * solar_size
-                * solar_efficiency
-                * 0.5
+                    self.solar_irradiance[SOLAR_IRRADIANCE][_t]
+                    * self.variables.solar_size
+                    * solar_efficiency
+                    * 0.5
             )
-            energy_demand_at_time = energy_demand.loc[_t, ENERGY_DEMAND]
-
             # Renewable electricity flow to house and battery must equal renewable generation
             self.problem += (
-                renewable_electricity_to_house[_t]
-                + renewable_electricity_to_battery[_t]
-                <= solar_generation
+                    self.variables.renewable_electricity_to_house[_t]
+                    + self.variables.renewable_electricity_to_battery[_t]
+                    <= solar_generation
             )
             # Electricity demand from house must be met
             self.problem += (
-                renewable_electricity_to_house[_t] + battery_electricity_to_house[_t]
-                == energy_demand_at_time
+                    self.variables.renewable_electricity_to_house[_t] + self.variables.battery_electricity_to_house[_t]
+                    == self.energy_demand.loc[_t, ENERGY_DEMAND]
             )
-
             # Battery state of charge
             if _t == 0:  # Initial battery state of charge
-                self.problem += battery_state_of_charge[_t] == battery_initial_capacity
+                self.problem += self.variables.battery_state_of_charge[_t] == battery_initial_capacity
             else:
                 # Battery state of charge must be equal to the previous state plus the electricity flow to the battery
                 # minus the electricity flow from the battery and the degradation
                 battery_degradation = (
-                    battery_degradation_rate * battery_state_of_charge[_t - 1]
+                        battery_degradation_rate * (1 - self.variables.battery_state_of_charge[_t - 1])
                 )
                 self.problem += (
-                    battery_state_of_charge[_t]
-                    == battery_state_of_charge[_t - 1]
-                    + renewable_electricity_to_battery[_t]
-                    - battery_electricity_to_house[_t]
-                    - battery_degradation
+                        self.variables.battery_state_of_charge[_t]
+                        == self.variables.battery_state_of_charge[_t - 1]
+                        + self.variables.renewable_electricity_to_battery[_t]
+                        - self.variables.battery_electricity_to_house[_t]
+                        - battery_degradation
                 )
             # Battery capacity must be greater than or equal to the battery state of charge
-            self.problem += battery_state_of_charge[_t] <= battery_capacity
+            self.problem += self.variables.battery_state_of_charge[_t] <= self.variables.battery_capacity
 
-        self.problem.solve()
+    def solve(self) -> None:
+        """
+        Method to solve the optimisation problem.
+        """
+        if self.problem is None:
+            raise ValueError("Optimisation problem has not been defined.")
+        else:
+            self.problem.solve()
+            logger.info(f"Status: {LpStatus[self.problem.status]}")
+            logger.info(f"Objective: {pl.value(self.problem.objective)}")
+            logger.info(f"Battery capacity: {pl.value(self.variables.battery_capacity)}")
+            logger.info(f"Solar size: {pl.value(self.variables.solar_size)}")
+            if self.optimisation_objective == OptimisationObjectives.MINIMISE_BATTERY_AND_SOLAR_COST:
+                logger.info(f"Total cost: {pl.value(self.problem.objective)}")
+                logger.info(f"Battery cost: {pl.value(self.variables.battery_capacity) * self.battery_capex}")
+                logger.info(f"Solar cost: {pl.value(self.variables.solar_size) * self.solar_capex}")
 
-        # Print results
-        print(f"Status: {LpStatus[self.problem.status]}")
-        print(f"Objective: {pl.value(self.problem.objective)}")
-        print(f"Battery capacity: {pl.value(battery_capacity)}")
-        print(f"Solar size: {pl.value(solar_size)}")
-        print(
-            f"Renewable electricity to house: {sum(pl.value(renewable_electricity_to_house[t]) for t in time_slices)}"
-        )
-        print(
-            f"Renewable electricity to battery: {sum(pl.value(renewable_electricity_to_battery[t]) for t in time_slices)}"
-        )
-        print(
-            f"Battery electricity to house: {sum(pl.value(battery_electricity_to_house[t]) for t in time_slices)}"
-        )
-        print(
-            f"Total House Demand: {sum(energy_demand.loc[t, ENERGY_DEMAND] for t in time_slices)}"
-        )
-
+    def plot_results(self) -> None:
+        """
+        Method to plot the results of the optimisation problem.
+        """
         # plot results
         plt.figure(figsize=(10, 5))
         plt.plot(
-            [pl.value(battery_state_of_charge[t]) for t in time_slices],
+            [pl.value(self.variables.battery_state_of_charge[t]) for t in self.time_slices],
             label="Battery state of charge",
         )
         plt.plot(
-            [pl.value(renewable_electricity_to_house[t]) for t in time_slices],
+            [pl.value(self.variables.renewable_electricity_to_house[t]) for t in self.time_slices],
             label="Renewable electricity to house",
         )
         plt.plot(
-            [pl.value(battery_electricity_to_house[t]) for t in time_slices],
+            [pl.value(self.variables.battery_electricity_to_house[t]) for t in self.time_slices],
             label="Battery electricity to house",
         )
         plt.plot(
             [
-                pl.value(battery_electricity_to_house[t])
-                + pl.value(renewable_electricity_to_house[t])
-                for t in time_slices
+                pl.value(self.variables.battery_electricity_to_house[t])
+                + pl.value(self.variables.renewable_electricity_to_house[t])
+                for t in self.time_slices
             ],
             label="Total electricity to house",
         )
         plt.plot(
             [
-                pl.value(battery_electricity_to_house[t])
-                + pl.value(renewable_electricity_to_house[t])
-                - energy_demand.loc[t, ENERGY_DEMAND]
-                for t in time_slices
+                pl.value(self.variables.battery_electricity_to_house[t])
+                + pl.value(self.variables.renewable_electricity_to_house[t])
+                - self.energy_demand.loc[t, ENERGY_DEMAND]
+                for t in self.time_slices
             ],
             label="Excess electricity",
         )
         plt.legend()
         plt.show()
 
+    def dump_results(self, output_path: Path) -> None:
+        """
+        Method to dump the results of the optimisation problem to a csv.
+        :param output_path: Path to dump the results to.
+        """
+        results = pd.DataFrame(
+            {
+                "battery_state_of_charge": [
+                    pl.value(self.variables.battery_state_of_charge[t])
+                    for t in self.time_slices
+                ],
+                "renewable_electricity_to_house": [
+                    pl.value(self.variables.renewable_electricity_to_house[t])
+                    for t in self.time_slices
+                ],
+                "battery_electricity_to_house": [
+                    pl.value(self.variables.battery_electricity_to_house[t])
+                    for t in self.time_slices
+                ],
+                "total_electricity_to_house": [
+                    pl.value(self.variables.battery_electricity_to_house[t])
+                    + pl.value(self.variables.renewable_electricity_to_house[t])
+                    for t in self.time_slices
+                ],
+                "excess_electricity": [
+                    pl.value(self.variables.battery_electricity_to_house[t])
+                    + pl.value(self.variables.renewable_electricity_to_house[t])
+                    - self.energy_demand.loc[t, ENERGY_DEMAND]
+                    for t in self.time_slices
+                ],
+                "battery_capacity": pl.value(self.variables.battery_capacity),
+                "solar_size": pl.value(self.variables.solar_size),
+            }
+        )
+        if self.optimisation_objective == OptimisationObjectives.MINIMISE_BATTERY_AND_SOLAR_COST:
+            results["total_cost"] = pl.value(self.problem.objective)
+            results["battery_cost"] = pl.value(self.variables.battery_capacity) * self.battery_capex
+            results["solar_cost"] = pl.value(self.variables.solar_size) * self.solar_capex
 
-if __name__ == "__main__":
-    energy_demand = pd.read_csv(
-        r"C:\Users\David.Wickham\PycharmProjects\solar_pv_and_battery_optimiser\energy_demand.csv"
-    )
-    solar_irradiance = pd.read_csv(
-        r"C:\Users\David.Wickham\PycharmProjects\solar_pv_and_battery_optimiser\solar_irradiance.csv"
-    )
-    optimiser = Optimiser()
-    optimiser.create_optimisation_problem(
-        energy_demand=energy_demand,
-        solar_irradiance=solar_irradiance,
-        battery_initial_capacity=0,
-        solar_capacity=10000000000000000000,
-        optimisation_objective=OptimisationObjectives.MINIMISE_BATTERY_CAP,
-        solar_efficiency=0.15,
-        battery_degradation_rate=0.01,
-        time_slices=range(len(energy_demand)),
-    )
+        results.to_csv(output_path, index=False)
+
+
+    def _define_problem(self):
+        """
+        Method to define and create the optimisation problem attribute.
+        """
+        if self.optimisation_objective == OptimisationObjectives.MINIMISE_BATTERY_CAP:
+            self.problem = pl.LpProblem("BatterySizeMinimisation", pl.LpMinimize)
+        elif (
+                self.optimisation_objective
+                == OptimisationObjectives.MINIMISE_BATTERY_AND_SOLAR_COST
+        ):
+            self.problem = pl.LpProblem("BatteryAndPVCostMinimisation", pl.LpMinimize)
+        else:
+            raise ValueError(
+                f"Unknown optimisation objective: {self.optimisation_objective}"
+            )
+
+    def _define_objective_function(
+            self,
+            battery_capex: Optional[float] = None,
+            solar_capex: Optional[float] = None,
+    ) -> None:
+        """
+        Method to define the objective function and to add to the problem attribute.
+        :param battery_capex: Battery capex (£/kWh)
+        :param solar_capex: Solar capex (£/m2)
+        :return: None
+        """
+        if self.optimisation_objective == OptimisationObjectives.MINIMISE_BATTERY_CAP:
+            if battery_capex:
+                logger.info("Battery capex has been provided as an input argument, however, the optimisation objective"
+                            "is to minimise battery size. Therefore, this battery capex will be ignored.")
+            if solar_capex:
+                logger.info("Solar capex has been provided as an input argument, however, the optimisation objective"
+                            "is to minimise battery size. Therefore, this solar capex will be ignored.")
+            self.problem += self.variables.battery_capacity
+        elif (
+                self.optimisation_objective
+                == OptimisationObjectives.MINIMISE_BATTERY_AND_SOLAR_COST
+        ):
+            self.problem += (
+                    self.variables.battery_capacity * battery_capex
+                    + self.variables.solar_size * solar_capex
+            )
+        else:
+            raise ValueError(
+                f"Unknown optimisation objective: {self.optimisation_objective}"
+            )
--- model/linear_optimiser/variables.py
+++ model/linear_optimiser/variables.py
@@ -1,4 +1,86 @@
+"""
+File containing all the variables for the linear optimiser
+"""
+import logging
+from dataclasses import dataclass, field
+from typing import Union
+
+import pulp as pl
+
+from model.constants import OptimisationObjectives
+
+logger = logging.getLogger(__name__)
+
+
+@dataclass
 class OptimiserVariables:
     """
     Class to store the variables for the optimiser
     """
+    renewable_electricity_to_house: pl.LpVariable = field(default_factory=pl.LpVariable, init=False)
+    renewable_electricity_to_battery: pl.LpVariable = field(default_factory=pl.LpVariable, init=False)
+    battery_electricity_to_house: pl.LpVariable = field(default_factory=pl.LpVariable, init=False)
+    battery_state_of_charge: pl.LpVariable = field(default_factory=pl.LpVariable, init=False)
+    battery_capacity: pl.LpVariable = field(default_factory=pl.LpVariable, init=False)
+    solar_size: Union[float, pl.LpVariable] = field(default_factory=pl.LpVariable, init=False)
+
+    @classmethod
+    def create_variables(
+            cls,
+            time_slices: range,
+            optimisation_objective: OptimisationObjectives,
+            solar_size: float = None,
+    ) -> "OptimiserVariables":
+        """
+        Function to create the variables for the optimiser.
+        :param time_slices: Time slices to optimise over
+        :param optimisation_objective: Optimisation objective
+        :param solar_size: Size of the solar array in m^2
+        :return: None
+        """
+        cls.renewable_electricity_to_house = pl.LpVariable.dicts(
+            name="renewable_electricity_to_house",
+            indices=time_slices,
+            lowBound=0,
+            cat="Continuous",
+        )
+        cls.renewable_electricity_to_battery = pl.LpVariable.dicts(
+            name="renewable_electricity_to_battery",
+            indices=time_slices,
+            lowBound=0,
+            cat="Continuous",
+        )
+        cls.battery_electricity_to_house = pl.LpVariable.dicts(
+            name="battery_electricity_to_house",
+            indices=time_slices,
+            lowBound=0,
+            cat="Continuous",
+        )
+        cls.battery_state_of_charge = pl.LpVariable.dicts(
+            name="battery_state_of_charge",
+            indices=time_slices,
+            lowBound=0,
+            cat="Continuous",
+        )
+        cls.battery_capacity = pl.LpVariable(
+            name="battery_capacity",
+            lowBound=0,
+            cat="Continuous",
+        )
+        if optimisation_objective == OptimisationObjectives.MINIMISE_BATTERY_AND_SOLAR_COST:
+            if solar_size:
+                logger.info(f"A solar size of {solar_size} was provided as an input argument, however, the"
+                            f" optimisation objective is to minimise battery and solar costs. Therefore, this solar"
+                            f" size will be ignored and the solar size will be optimised over.")
+            cls.solar_size = pl.LpVariable(
+                name="solar_capacity",
+                lowBound=0,
+                cat="Continuous",
+            )
+        elif optimisation_objective == OptimisationObjectives.MINIMISE_BATTERY_CAP:
+            cls.solar_size = solar_size
+        else:
+            raise ValueError(
+                f"Unknown optimisation objective: {optimisation_objective}"
+            )
+        return cls
