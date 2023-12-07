ce0765788fe7268ed421211ee1d2ec4ef03560da
davidwickh <56641696+davidwickh@users.noreply.github.com>
2023-12-07 21:49:31 +0000

    Adding tests for the optimiser variables

---
+++ tests/unit/test_optimiser_variables.py
@@ -0,0 +1,35 @@
+"""
+This file contains tests for the optimiser_variables module.
+"""
+
+import pytest
+from model.constants import OptimisationObjectives
+from model.linear_optimiser.variables import OptimiserVariables
+
+
+class TestOptimiserVariables:
+    """
+    Class containing tests for the OptimiserVariables class.
+    """
+
+    @pytest.fixture(scope="class")
+    def optimiser_variables(self) -> OptimiserVariables:
+        """
+        Creates an OptimiserVariables object.
+        :return: OptimiserVariables object
+        """
+        return OptimiserVariables.create_variables(
+            time_slices=range(10),
+            optimisation_objective=OptimisationObjectives.MINIMISE_BATTERY_CAP,
+            solar_size=10
+        )
+
+    def test_create_variables(self, optimiser_variables: OptimiserVariables) -> None:
+        """
+        Tests that the variables are created correctly.
+        """
+        assert len(optimiser_variables.renewable_electricity_to_house) == 10
+        assert len(optimiser_variables.renewable_electricity_to_battery) == 10
+        assert len(optimiser_variables.battery_electricity_to_house) == 10
+        assert len(optimiser_variables.battery_state_of_charge) == 10
+        assert optimiser_variables.solar_size == 10
