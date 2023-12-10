f350801d40eb78bf10d4d9c281be97139030850b
davidwickh <56641696+davidwickh@users.noreply.github.com>
2023-12-10 22:36:46 +0000

    Improvements to the pre-processing

--- model/inputs/pre_processing.py
+++ model/inputs/pre_processing.py
@@ -4,7 +4,7 @@ This file contains all code associated with pre-processing the input data.
 
 import pandas as pd
 
-from model.constants import DATE_TIME, SOLAR_IRRADIANCE
+from model.constants import DATE_TIME, SOLAR_IRRADIANCE, ENERGY_DEMAND
 
 
 class PreProcessorBase:
@@ -15,17 +15,68 @@ class PreProcessorBase:
     def __init__(self):
         pass
 
-    def pre_process(self, data: pd.DataFrame) -> pd.DataFrame:
+    def pre_process(self, data: pd.DataFrame, column: str) -> pd.DataFrame:
         """
-        Converts the timestamp column to a datetime object.
+        Base pre-processing method. The following steps are applied:
+            - The timestamp column is converted to a datetime object.
+            - Duplicate timestamps are processed.
+            - Data is re-sampled to half-hourly.
         :param data: Input data to pre-process
         :return: Pre-processed data
         """
-        data[DATE_TIME] = pd.to_datetime(data[DATE_TIME], format="mixed")
+        data[DATE_TIME] = pd.to_datetime(data[DATE_TIME], format="%d/%m/%Y %H:%M")
+        data = self._process_duplicate_timestamps(data, column=column)
+
         return data
 
+    @staticmethod
+    def _process_duplicate_timestamps(data: pd.DataFrame, column: str) -> pd.DataFrame:
+        """
+        Method that processes duplicate timestamps in a dataframe. If duplicate timestamps have the same values then
+        duplicate rows are dropped. If duplicate timestamps have different values then the average of the values is
+        taken.
 
-class FilterNonHalfHourlyData(PreProcessorBase):
+        :param data: Dataframe to process
+        :param column: Column to process
+        """
+        # Find duplicate rows based on the timestamp column
+        duplicate_rows = data[data[DATE_TIME].duplicated(keep=False)]
+        # If there are no duplicate rows then return the original dataframe
+        if duplicate_rows.empty:
+            return data
+        # Get the unique timestamps
+        unique_timestamps = duplicate_rows[DATE_TIME].unique()
+        # Drop the duplicate timestamps from the original dataframe
+        data = data[~data[DATE_TIME].isin(unique_timestamps)]
+        # Create a dataframe to store the processed data
+        processed_data = pd.DataFrame(columns=data.columns)
+        # Iterate over the unique timestamps
+        for timestamp in unique_timestamps:
+            # Get the rows with the timestamp
+            rows = duplicate_rows[duplicate_rows[DATE_TIME] == timestamp]
+            # If there is only one row then add it to the processed data
+            if len(rows) == 1:
+                processed_data = processed_data._append(rows)
+            # If there are multiple rows then process the rows
+            else:
+                # Get the unique values
+                unique_values = rows[column].unique()
+                # If there is only one unique value then add the row to the processed data
+                if len(unique_values) == 1:
+                    processed_data = processed_data._append(rows.drop_duplicates())
+                # If there are multiple unique values then take the average of the values and add the row to the processed
+                # data
+                else:
+                    rows[column] = rows[column].mean()
+                    processed_data = processed_data._append(rows.drop_duplicates())
+        # Combine the processed data with the original data
+        processed_data = pd.concat([processed_data, data])
+        # Sort the index
+        processed_data.sort_values(by=DATE_TIME, inplace=True)
+        return processed_data
+
+
+class PreProcessEnergyDemand(PreProcessorBase):
     """
     Pre-processor that filters out non-half-hourly data.
 
@@ -41,11 +92,11 @@ class FilterNonHalfHourlyData(PreProcessorBase):
         :param data: Input data to pre-process
         :return: Pre-processed data
         """
-        data = super().pre_process(data)
+        data = super().pre_process(data, column=ENERGY_DEMAND)
         return data[data[DATE_TIME].dt.minute.isin([0, 30])]
 
 
-class MatchTimeStampsPreProcessor(PreProcessorBase):
+class PreProcessSolarIrradiance(PreProcessorBase):
     """
     Pre-processor that matches the timestamps of the solar irradiance and energy demand data.
 
@@ -71,22 +122,27 @@ class MatchTimeStampsPreProcessor(PreProcessorBase):
         :return: Pre-processed data
         """
         # Convert the timestamp column to a datetime object
-        data_with_converted_timestamp = super().pre_process(data)
+        data_with_converted_timestamp = super().pre_process(data, column=SOLAR_IRRADIANCE)
 
-        data_with_converted_timestamp.set_index(DATE_TIME, inplace=True)
-        orginal_columns = data_with_converted_timestamp.columns
+        data_with_converted_timestamp = self.fill_in_zero_values(data_with_converted_timestamp)
+
+        data_with_converted_timestamp = data_with_converted_timestamp.set_index(DATE_TIME)
+
         # re-sample the solar irradiance data to half-hourly
-        data_with_resampled_timestamp = data_with_converted_timestamp.resample(
+        data_with_converted_timestamp = data_with_converted_timestamp.resample(
             "30min"
         ).bfill()
+
+        orginal_columns = data_with_converted_timestamp.columns
+
         # Need to add the very last 30 minutes of the year to the end of the data
-        new_timestamp = data_with_resampled_timestamp.index[-1] + pd.Timedelta(
+        new_timestamp = data_with_converted_timestamp.index[-1] + pd.Timedelta(
             minutes=30
         )
-        last_value = data_with_resampled_timestamp.iloc[-1][SOLAR_IRRADIANCE]
+        last_value = data_with_converted_timestamp.iloc[-1][SOLAR_IRRADIANCE]
         new_row = pd.DataFrame({SOLAR_IRRADIANCE: [last_value]}, index=[new_timestamp])
         data_with_resampled_timestamp = pd.concat(
-            [data_with_resampled_timestamp, new_row]
+            [data_with_converted_timestamp, new_row]
         )
 
         # Get the years of the energy demand data and the year of the solar irradiance data
@@ -137,3 +193,51 @@ class MatchTimeStampsPreProcessor(PreProcessorBase):
         shifted_and_repeated_solar_irradiance.reset_index(inplace=True)
 
         return shifted_and_repeated_solar_irradiance
+
+    @staticmethod
+    def fill_in_zero_values(solar_irradiance: pd.DataFrame) -> pd.DataFrame:
+        """
+        Method to loop through each day, compare the solar irradiance values to the previous day and if the solar
+        irradiance values are zero for any time period that the previous day had values, then the solar irradiance
+        values are replaced with the previous day's values.
+        """
+        days = solar_irradiance[DATE_TIME].dt.date.unique()
+        # Loop through each day
+        for day in days:
+            # Get the solar irradiance data for the day
+            solar_irradiance_day = solar_irradiance[
+                solar_irradiance[DATE_TIME].dt.date == day
+                ]
+            # Get the previous day
+            previous_day = day - pd.Timedelta(days=1)
+            # Get the solar irradiance data for the previous day
+            solar_irradiance_previous_day = solar_irradiance[
+                solar_irradiance[DATE_TIME].dt.date == previous_day
+                ]
+            if solar_irradiance_previous_day.empty:
+                continue
+            # Loop through each time period
+            for time_period in solar_irradiance_day[DATE_TIME].dt.time.unique():
+                # Get the solar irradiance value for the time period
+                solar_irradiance_value = solar_irradiance_day[
+                    solar_irradiance_day[DATE_TIME].dt.time == time_period
+                    ][SOLAR_IRRADIANCE].values[0]
+                # If the solar irradiance value is zero
+                if solar_irradiance_value == 0:
+                    # Get the solar irradiance value for the previous day
+                    solar_irradiance_previous_day_value = solar_irradiance_previous_day[
+                        solar_irradiance_previous_day[DATE_TIME].dt.time == time_period
+                        ][SOLAR_IRRADIANCE].values[0]
+                    # If the solar irradiance value for the previous day is not zero then replace the value
+                    if solar_irradiance_previous_day_value != 0:
+                        solar_irradiance.loc[
+                            (
+                                    solar_irradiance[DATE_TIME].dt.date == day
+                            )
+                            & (
+                                    solar_irradiance[DATE_TIME].dt.time
+                                    == time_period
+                            ),
+                            SOLAR_IRRADIANCE,
+                        ] = solar_irradiance_previous_day_value
+        return solar_irradiance
